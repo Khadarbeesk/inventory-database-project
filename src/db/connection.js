75ed3abb54c2038c05f1fcf64bba21a5dad252c2
khadarbeesk <khadarbeesk52@gmail.com>
2026-04-09 13:07:34 +0530

    fix db undefined error

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,10 @@
 const sqlite3 = require("sqlite3").verbose();
 
-new sqlite3.Database(__dirname + "/database.db", (err) => {
+// ✅ store in variable
+const db = new sqlite3.Database(__dirname + "/database.db", (err) => {
   if (err) console.error(err.message);
   else console.log("Connected to SQLite DB");
 });
 
 module.exports = db;
+
