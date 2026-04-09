d527e56857b6cd0c813d4bde1e4b06e738a4022f
khadarbeesk <khadarbeesk52@gmail.com>
2026-04-09 13:18:31 +0530

    added more sample data

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -12,3 +12,23 @@ CREATE TABLE IF NOT EXISTS inventory (
   price REAL NOT NULL,
   FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
 );
+INSERT INTO suppliers (name, city) VALUES
+('HP', 'Pune'),
+('Lenovo', 'Mumbai');
+
+-- More inventory data
+INSERT INTO inventory (supplier_id, product_name, quantity, price) VALUES
+(1, 'Sofa', 4, 15000),
+(1, 'Bed', 2, 20000),
+
+(2, 'Wardrobe', 3, 12000),
+(2, 'Table', 7, 6500),
+
+(3, 'Monitor', 6, 12000),
+(3, 'Keyboard', 15, 1500),
+
+(4, 'Laptop', 5, 48000),
+(4, 'Printer', 2, 9000),
+
+(5, 'Laptop', 7, 52000),
+(5, 'Mouse', 20, 800);
